f76e894870a21c80a732c48c5a403a5743db3e3d
rafi ahamed <cityinstitute1992@gmail.com>
2021-02-24 01:52:26 +0600

    array for training

---
+++ array slice map.go
@@ -0,0 +1,19 @@
+package main 
+
+import "fmt"
+
+func main( ){
+//var students [3]string
+//students[0]="rafi"
+//students[1]="simanto"
+//students[2]="maruf"
+
+
+ //fmt.Println(students)
+ //fmt.Println(len(students))
+ //fmt.Println(students[2])
+//}
+ //array string literal:for representing a fixed value in source code
+students:=[...]string{"rafi","simanto","maruf","maruf 2","raju" }
+ fmt.Println(students,len(students))
+}
